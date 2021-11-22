fe49d266331e3f6251bac47f299365810c13998e
Emerson Junqueira <emoonlit@gmail.com>
2021-11-22 18:27:17 -0300

    refactor: global.d.ts

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,11 +1,11 @@
-import supertest from "supertest";
-
 describe('Beach forecast functional tests', () => {
   it('should return a forecast with just a few times', async () => {
-    const { body, status } = await supertest(app).get('/forecast');
+    const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
-    expect(body).toBe([{
-      "teste": "teste"
-    }])
-  })
-})
+    expect(body).toEqual([
+      {
+        tme: '2020',
+      },
+    ]);
+  });
+});
